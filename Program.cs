e9839dbe420fde5014551fe6ddff0917bae425dc
AffanRidha <affan.ridha02@gmail.com>
2021-03-18 00:33:38 +0700

    membuat projek

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+
+            if (OnDuty == true)
+            {
+                Console.WriteLine("OnDuty : Yes", OnDuty);
+            }else
+            {
+                Console.WriteLine("OnDuty : No", OnDuty);
+            }
+            Console.WriteLine("Number Of Passenger : {0}\n", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} Sedang Menjemput Penumpang", DriverName);
+        }
+        public void DropOfPassenger()
+        {
+            Console.WriteLine("{0} Selesai Menjemput Penumpang\n", DriverName);
+        }
+    }
+}
